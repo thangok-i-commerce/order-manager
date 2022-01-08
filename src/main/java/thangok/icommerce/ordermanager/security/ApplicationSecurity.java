6599ce954f5665db696af2abdc064336a7f9eb62
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-08 20:40:16 +0700

    add webflux security

--- src/main/java/thangok/icommerce/ordermanager/security/ApplicationSecurity.java
+++ src/main/java/thangok/icommerce/ordermanager/security/ApplicationSecurity.java
@@ -18,12 +18,16 @@ public class ApplicationSecurity {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange()
+                .pathMatchers("/**").permitAll()
                 .pathMatchers("/home/user").hasAnyRole("USER", "ADMIN")
                 .pathMatchers("/home/admin").hasRole("ADMIN")
                 .anyExchange()
                 .authenticated()
                 .and()
                 .formLogin();
+
+        http.csrf(csrf -> csrf.disable());
+
         return http.build();
     }
 
